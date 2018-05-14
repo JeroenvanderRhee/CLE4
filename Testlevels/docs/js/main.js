d0db46a3cc37699f5d6b5449842c775ed756c564
JeroenvanderRhee <32259558+JeroenvanderRhee@users.noreply.github.com>
2018-05-14 12:03:58 +0200

    3.0.2
    Mappen structuur

---
+++ Testlevels/docs/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+console.log("hoi");
+var Blok = (function () {
+    function Blok() {
+        this.speed = 10;
+        this.width = 300;
+        this.height = 200;
+        this.BeginPositionX = 0;
+        this.BeginPositionY = 0;
+        this.currentLocationX = 0;
+        this.currentLocationY = 0;
+        console.log("gelukt");
+    }
+    return Blok;
+}());
+//# sourceMappingURL=main.js.map
